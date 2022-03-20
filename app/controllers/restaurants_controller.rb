775ea6cfe584be606ad51af5a897da70589b5e50
Teddy Bandama <bandama.teddy@gmail.com>
2022-03-20 14:22:51 +0100

    mvc ok , add delete ok , link_to update not ok

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,10 +1,11 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+
   def index
     @restaurants = Restaurant.all
   end
 
   def show
-    @restaurant = Restaurant.find(params[:id])
   end
 
   def new
@@ -12,20 +13,34 @@ class RestaurantsController < ApplicationController
   end
 
   def create
-    Restaurant.create(params[:restaurant])
-    #@restaurant = Restaurant.new(restaurant_params)
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
   def edit
-    @restaurant = Restaurant.find(params[:id])
   end
 
   def update
-    @restaurant = Restaurant.find(params[:id])
-    @restaurant.update(params[:restaurant])
+    @restaurant.update(restaurant_params)
+    redirect_to restaurant_path(@restaurant)
   end
 
   def destroy
+    @restaurant.destroy
+    redirect_to restaurants_path
   end
 
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :adress, :stars)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
 end
